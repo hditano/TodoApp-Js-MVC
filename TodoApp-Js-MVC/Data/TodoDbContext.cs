92bf5e55c17d547ee2d1fbb45169ad5460defff5
Hernan Di Tano <hditano@gmail.com>
2023-05-18 02:32:40 +0200

    Add project files.

---
+++ TodoApp-Js-MVC/Data/TodoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp_Js_MVC.Models;
+
+namespace TodoApp_Js_MVC.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options ) : base(options)
+        {
+            
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ TodoApp-Js-MVC/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApp_Js_MVC.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int TodoID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
