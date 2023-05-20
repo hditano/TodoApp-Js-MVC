1951de2515919af945e1e5a86ac82067347391f9
Hernan Di Tano <hditano@gmail.com>
2023-05-20 13:29:15 +0200

    Added basic JS functionality to call AllItems while click on Button

---
+++ TodoApp-Js-MVC/wwwroot/js/script.js
@@ -0,0 +1,11 @@
+﻿const uri = 'https://localhost:7047/api/Todo'
+let todos = [];
+
+function GetItems() {
+    console.log('Hello World')
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => console.log(data));
+
+}
+
